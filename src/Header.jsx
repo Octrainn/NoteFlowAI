730190aaa3c05e385f0b25c7cd79b4a56d894062
Octrainn <akshay2334@hotmail.com>
2023-08-30 15:27:16 -0400

    set up navbar and header jsx files

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CSS Files/Header.css"
+
+function Header() {
+    return (
+        <div>
+           <h3>test</h3>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CSS Files/Navbar.css"
+
+
+function Navbar (){
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+export default Navbar;
