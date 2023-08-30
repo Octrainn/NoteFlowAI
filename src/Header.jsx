361f789d40bfd33b1b5cceeb0660765b2cd0d424
Octrainn <akshay2334@hotmail.com>
2023-08-30 16:52:54 -0400

    navbar semi-completely, need to add functionality, and fix top navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import "./CSS Files/Header.css"
 function Header() {
     return (
         <div>
-           <h3>test</h3>
+          
         </div>
     );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,10 +3,44 @@ import "./CSS Files/Navbar.css"
 
 
 function Navbar (){
+
+    const list = [        
+        "New Page", 
+        "Journal",
+        "Theme",
+        "Diary",
+        "Template (Notebook vs Blank)"
+
+    ]
+
+    const list2 = [
+      "Text Size",
+      "Text Style",
+      "Text Color",
+      "Add Bullet Points",
+      "Line Spacing",
+      
+    ]
+
     return (
-        <div>
-          
-        </div>
+        <>
+        <nav className="top-nav">
+          <ul>
+          {list2.map(item => (
+                    <li className="top-row-elements" key={item}>{item}</li>
+                ))}
+          </ul>
+
+
+        </nav>
+          <nav className="nav-container">
+            <ul>
+            {list.map(item => (
+                    <li className="elements" key={item}>{item}</li>
+                ))}
+            </ul>
+          </nav>
+        </> 
     )
 }
 
