6d0b85dc47a669a488ed3214582b25fbdc8dab4f
Octrainn <akshay2334@hotmail.com>
2023-09-04 22:07:13 -0400

    made sidebar more asthetically pleasing, icons added, maybe update them, right bar tommorow

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./CSS Files/Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faBook, faFile, faPaintBrush, faCopy} from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
     const list = [        
         "New Page",
         "Journal",
         "Theme",
-        "Diary",
+       
         "Template"
     ];
 
@@ -19,8 +19,13 @@ function Navbar() {
                     {list.map(item => (
                         <li className="elements" key={item}>
                             <span className="icon-text">
-                                {item === "New Page" && <FontAwesomeIcon icon={faPlus} className="plus-icons" />}
+                                {item === "New Page" && <FontAwesomeIcon icon={faFile} className="plus-icons" />}
                                 {item === "Journal" && <FontAwesomeIcon icon={faBook} className="plus-icons" />}
+                                {item === "Theme" && <FontAwesomeIcon icon={faPaintBrush} className="plus-icons" />}
+                                {item === "Template" && <FontAwesomeIcon icon={faCopy} className="plus-icons" />}
+                               
+                               
+                               
                                 {item}
                             </span>
                         </li>
