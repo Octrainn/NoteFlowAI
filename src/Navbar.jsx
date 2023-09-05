ea5c1fb3e75cbe3b71dd9b75841691f08f19bf64
Octrainn <akshay2334@hotmail.com>
2023-09-04 20:49:45 -0400

    merged header and navbar jsx, app container for both files so we can tweak both

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-import "./CSS Files/Navbar.css"
+import "./CSS Files/Navbar.css";
 import Header from "./Header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-
-function Navbar (){
-
+function Navbar () {
     const list = [        
-        "New Page", 
+        "New Page",
         "Journal",
         "Theme",
         "Diary",
         "Template"
-
-    ]
-
-   
+    ];
 
     return (
         <>
-    
-          <nav className="nav-container">
-            <ul>
-            {list.map(item => (
-                    <li className="elements" key={item}>{item}</li>
-                ))}
-            </ul>
-          </nav>
-        </> 
-    )
+        <div className="app-container">
+
+       
+            <nav className="nav-container">
+                <ul>
+                    {list.map(item => (
+                        <li className="elements" key={item}>
+                            <span className="icon-text">
+                                {item}
+                                {item === "New Page" && <FontAwesomeIcon icon={faPlus} className="plus-icon" />}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <Header />
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
